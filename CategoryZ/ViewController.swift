a0bbdf067cb66fee393e6e7500e4935b3f36e339
judge_happily <you3192001@gmail.com>
2024-04-09 23:40:24 +0900

    Initial Commit

---
+++ CategoryZ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CategoryZ
+//
+//  Created by Jae hyung Kim on 4/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
