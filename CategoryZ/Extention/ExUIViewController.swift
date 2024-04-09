491c8daf9495ca8a5b32a2fae3f029b295707f9f
judge_happily <you3192001@gmail.com>
2024-04-10 02:05:52 +0900

    Fetch Extention Reactivw For ViewConroller

--- CategoryZ/Extention/ExUIViewController.swift
+++ CategoryZ/Extention/ExUIViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 extension UIViewController {
     
@@ -19,4 +21,31 @@ extension UIViewController {
         sceneDelegate?.window?.makeKey()
     }
     
+}
+
+extension Reactive where Base: UIViewController {
+    
+    var viewWillAppear: ControlEvent<Bool> {
+        let source = self.methodInvoked(#selector(base.viewWillAppear))
+            .map { $0.first as? Bool ?? false }
+        return ControlEvent(events: source)
+    }
+    
+    var viewDidAppear: ControlEvent<Bool> {
+        let source = self.methodInvoked(#selector(base.viewWillDisappear))
+            .map { $0.first as? Bool ?? false }
+        return ControlEvent(events: source)
+    }
+    
+    var viewWillDisapear: ControlEvent<Bool> {
+        let source = self.methodInvoked(#selector(base.viewWillDisappear))
+            .map { $0.first as? Bool ?? false }
+        return ControlEvent(events: source)
+    }
+    
+    var viewDidDisapear: ControlEvent<Bool> {
+        let source = self.methodInvoked(#selector(Base.viewDidDisappear))
+            .map { $0.first as? Bool ?? false }
+        return ControlEvent(events: source)
+    }
 }
