8638463d96b04dffc04b8187a7248c3037c8f1be
judge_happily <you3192001@gmail.com>
2024-05-16 19:36:30 +0900

    Fetch: RxOnlyRotateTableView #Re

--- CategoryZ/View/Base/RxOnlyRotateTableView.swift
+++ CategoryZ/View/Base/RxOnlyRotateTableView.swift
@@ -8,9 +8,50 @@
 import UIKit
 import SnapKit
 import Then
+import RxSwift
+import RxCocoa
 
 final class RxOnlyRotateTableView: RxBaseView {
     let tableView = UITableView().then {
         $0.transform = CGAffineTransform(scaleX: 1, y: -1)
+    }
+    let commentTextView = CommentTextView(placeholedrText: "메시지를 남기세요")
+    
+    
+    private
+    func subscribe() {
+        autoResizingTextView()
+    }
+    
+    override func configureHierarchy() {
+        addSubview(tableView)
+        addSubview(commentTextView)
+    }
+    
+    override func configureLayout() {
+        tableView.snp.makeConstraints { make in
+            make.horizontalEdges.top.equalToSuperview()
+            make.bottom.equalTo(commentTextView.snp.top)
+        }
+        commentTextView.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(safeAreaLayoutGuide)
+            make.bottom.greaterThanOrEqualTo(keyboardLayoutGuide.snp.bottom)
+        }
+    }
+}
+
+
+extension RxOnlyRotateTableView {
+    private
+    func autoResizingTextView() {
+        commentTextView.textView.rx.text.orEmpty
+            .observe(on: MainScheduler.instance)
+            .bind(with: self) { owner, text in
+                let size = CGSize(width: owner.commentTextView.textView.frame.width, height: CGFloat.infinity)
+                let estimate = owner.commentTextView.textView.sizeThatFits(size)
+
+                owner.commentTextView.textViewHeightConstraint?.update(offset: max(40,min(estimate.height, 120)))
+            }
+            .disposed(by: disposedBag)
     }
 }
---
+++ CategoryZ/ViewController/Chatting/ChattingViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ChattingViewController.swift
+//  CategoryZ
+//
+//  Created by Jae hyung Kim on 5/16/24.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class ChattingViewController: RxHomeBaseViewController<RxOnlyRotateTableView> {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    
+    override func subscribe() {
+        
+    }
+    
+}
+
